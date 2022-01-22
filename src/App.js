2a2b6d21d5ccf968b3d25cfc8bf72b9ebd8a4ca4
Olga Vlasova <73309221+OlgaVlasova-IT@users.noreply.github.com>
2022-01-21 20:25:09 -0700

    update 22022-01-21 8:25 pm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import veggie from './vegetables.png';
-// import fruit from './fruits.png'
-// import grains from './grains.png'
-// import protein from './protein.png'
 import './App.css';
 import List from './List';
 
@@ -10,11 +7,8 @@ function App() {
     <div className="App">
 
       <div className="contLogo">
-      {/* <img src={fruit} className="logo" alt="fruit" width="100" height="100"  /> */}
-         <img src={veggie} className="logoRotate" alt="veggie" width="100" height="100"  /> 
-        {/* <img src={grains} className="logo" alt="grains logo" width="100" height="100"  />
-         <img src={protein} className="logo" alt="protein logo" width="100" height="100"  /> */}
-        </div>
+       <img src={veggie} className="logoRotate" alt="veggie" width="100" height="100"  /> 
+      </div>
         <div className="App-header"> 
         <h2 className="par1">Grocery List</h2>
         <List/>
